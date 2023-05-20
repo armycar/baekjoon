ee8cb659d2807ec8db4bcdffea5144992bf999d1
armycar <credit102@naver.com>
2023-05-20 12:17:37 +0900

    first

---
+++ src/q10869.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class q10869 {
+    public static void main(String[] args) throws Exception {
+        int a, b;
+        Scanner scanner = new Scanner(System.in);
+
+        a = scanner.nextInt();
+
+        b = scanner.nextInt();
+
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+
+        scanner.close();
+    }
+}
