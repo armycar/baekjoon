038fcb3ddc8cdcc37ac66d46088cb51e071a8282
armycar <credit102@naver.com>
2023-05-22 16:41:21 +0900

    230522

---
+++ src/L1/q11382.java
@@ -0,0 +1,16 @@
+package L1;
+import java.util.*;
+public class q11382 {
+    public static void main(String[] args) throws Exception {
+        Long a, b, c;
+        Scanner scanner = new Scanner(System.in);
+
+        a = scanner.nextLong();    
+        b = scanner.nextLong();
+        c = scanner.nextLong();
+
+        System.out.println(a+b+c);
+   
+        scanner.close();
+    }
+}
