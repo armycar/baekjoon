6cf9f7b27475ae7cd7e2445b28e6c5f9fc9875ee
armycar <credit102@naver.com>
2023-05-29 19:44:26 +0900

    230529

--- src/L3/q15552.java
+++ src/L3/q15552.java
@@ -2,6 +2,7 @@ package L3;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.Scanner;
@@ -9,11 +10,26 @@ import java.util.Scanner;
 public class q15552 {
 public static void main(String[] args) {
     {
+        try {
        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+        
 
-       writer.write();
+       int n = Integer.parseInt(reader.readLine());
+       int sum = 0;
+       for(int i=1;i<=n;i++) {
+        String[] input = reader.readLine().split(" ");
+        int A = Integer.parseInt(input[0]);
+        int B = Integer.parseInt(input[1]);
+        sum = A+B;
+        writer.write(String.valueOf(sum));
+        writer.newLine();
+       }
        writer.flush();
+        }
+        catch(IOException e) {
+            e.printStackTrace();
+        }
 }
 }
 }
