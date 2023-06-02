ddbc6baff3f8a5d8ead94c763e0e110bad43510f
armycar <credit102@naver.com>
2023-06-02 16:50:38 +0900

    230602

---
+++ src/L3/q10951.java
@@ -0,0 +1,17 @@
+package L3;
+
+import java.util.Scanner;
+
+public class q10951 {
+public static void main(String[] args) {
+    {
+        Scanner scanner = new Scanner(System.in);
+        while(scanner.hasNextInt()) {
+            int a = scanner.nextInt();
+            int b = scanner.nextInt();
+            System.out.println(a+b);
+        }
+        scanner.close();
+    }
+}
+}
