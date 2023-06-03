f7bf47b3d315fda5c6f4bd1a68bd5e6f0c7f1607
armycar <credit102@naver.com>
2023-06-03 21:26:03 +0900

    230603

---
+++ src/L4/q10807.java
@@ -0,0 +1,15 @@
+package L4;
+
+import java.util.Scanner;
+
+public class q10807 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        int[] num = new int[N];
+
+        for(int i=0;i<=N;i++) {
+            num[i] = scanner.nextInt();
+        }
+    }
+}
